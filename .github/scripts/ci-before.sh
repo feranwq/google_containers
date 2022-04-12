a7256503ec47a9ffa2d666627d108277636cf548
feranwq <feranwq@gmail.com>
2022-04-12 17:38:06 +0800

    fix: fix ci err

--- .github/scripts/ci-before.sh
+++ .github/scripts/ci-before.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-
 mkdir -p ${HOME}/sync
 
 [[ -z "$USE_DOCKER_BIN" ]] && {
   cp imgsync ${HOME}/sync/
 } || {
   # 出现bug的时候直接使用docker的二进制文件，而不用提交代码触发ci
-  docker run --rm -tid --name tool zhangguanzhang/google_containers_imgsync top
+  docker run --rm -tid --name tool ${DOCKER_USER}/google_containers_imgsync top
   docker cp tool:/imgsync ${HOME}/sync/
   docker kill tool
 }
 
-docker run --rm -tid --name db zhangguanzhang/google_containers_db top
+docker run --rm -tid --name db ${DOCKER_USER}/google_containers_db top
 docker cp db:/bolt.db ${HOME}/sync/
 
 docker kill db
